ea7773fc65310af400179edd5b8a6d25350e025a
Adam Prochazka <62314569+adamp02@users.noreply.github.com>
2024-11-21 20:42:04 -0500

    Basic movement, grounding raycast, and anim

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float moveSpeed = 10f;
+    public float groundDist = 2f; // for raycast
+    public LayerMask groundLayer;
+    Rigidbody2D rb;
+
     public enum FacingDirection
     {
         left, right
@@ -12,7 +18,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,26 +26,56 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
 
+        rb.AddForce(transform.right * playerInput * moveSpeed);
+
+        // Draw the groundCheck Raycast
+        // https://docs.unity3d.com/ScriptReference/Debug.DrawRay.html
+        Vector3 groundCheck = transform.TransformDirection(-Vector3.up) * groundDist;
+        Debug.DrawRay(transform.position, groundCheck, Color.red);
+
+
+        if (!IsGrounded())
+        {
+            Debug.Log("Not grounded!");
+           // rb.AddForce(-transform.up * 5f);
+        }
     }
 
     public bool IsWalking()
     {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            return true;
+        }
+
         return false;
     }
     public bool IsGrounded()
     {
-        return true;
+        bool isGrounded = Physics2D.Raycast(transform.position, -transform.up, groundDist, groundLayer);
+        return isGrounded;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (rb.velocity.x < 0.1)
+        {
+            return FacingDirection.left;
+        }
+
+        if (rb.velocity.x > 0.1)
+        {
+            return FacingDirection.right;
+        }
+
+        // better to get the previous valid direction instead
+        return FacingDirection.left; 
     }
 }
