4fb3e2b9456d36cb44b95548900232597246be02
Adam Prochazka <62314569+adamp02@users.noreply.github.com>
2024-11-22 16:18:16 -0500

    Jumping and velocity update

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 10f;
+    public float maxSpeed = 10f;
+    public float timeToReachMaxSpeed = 1f;
+
+    public float decelSpeed = 1f;
+    public float timeDecel = 1f;
+
+    private float acceleration;
+    private float deceleration;
     public float groundDist = 2f; // for raycast
     public LayerMask groundLayer;
     Rigidbody2D rb;
+
+    public float apexHeight = 1f;
+    public int apexTime = 1;
+
+    public float calculatedGrav = 0f;
+
+    public float terminalSpeed = 4f;
 
     public enum FacingDirection
     {
@@ -18,22 +32,50 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        acceleration = maxSpeed / timeToReachMaxSpeed;
+        deceleration = decelSpeed / timeDecel;
         rb = GetComponent<Rigidbody2D>();
+        calculatedGrav = 2 * apexHeight / ((apexTime) ^ 2);
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         MovementUpdate(playerInput);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+            Debug.Log("TEST");
+        }
+
+        acceleration = maxSpeed / timeToReachMaxSpeed;
+        deceleration = decelSpeed / timeDecel;
+
     }
+
+
+    private void Jump()
+    {
+        Vector2 test = new Vector2(0f, (9 * (apexHeight / apexTime)));
+        rb.AddForce(transform.up * test, ForceMode2D.Impulse);
+    }
+
 
     private void MovementUpdate(Vector2 playerInput)
     {
 
-        rb.AddForce(transform.right * playerInput * moveSpeed);
+        // rb.AddForce(transform.right * playerInput * moveSpeed);
+        rb.velocity += Vector2.right * playerInput * (acceleration * Time.deltaTime);
+
+        if (playerInput.x == 0 && rb.velocity.x > 0.1f || rb.velocity.x < 0.1f)
+        {
+           // rb.velocity -= Vector2.right * deceleration * Time.deltaTime;
+        }
 
         // Draw the groundCheck Raycast
         // https://docs.unity3d.com/ScriptReference/Debug.DrawRay.html
@@ -44,8 +86,16 @@ public class PlayerController : MonoBehaviour
         if (!IsGrounded())
         {
             Debug.Log("Not grounded!");
-           // rb.AddForce(-transform.up * 5f);
+            rb.AddForce(-transform.up * calculatedGrav);
+            rb.gravityScale = calculatedGrav;
+        } else
+        {
+            rb.gravityScale = 0;
         }
+
+        // terminal velocity
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, terminalSpeed);
+
     }
 
     public bool IsWalking()
